51f2ec0220dea88408d8b47f84f4a8cef2ab27a5
Fernando Jimenez <fgjv06@gmail.com>
2023-04-09 18:31:41 -0400

    JS - submitBtn addEventListener

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let submitBtn = document.getElementById("submit-btn");
 
 let generateGif = () => {};
+
+// Generate Gifs on screen load or when user clicks on submit
+submitBtn.addEventListener("click, generateGif");
