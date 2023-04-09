ac009f0d5bb3b202b1aeba81b17e50b07f12126f
Fernando Jimenez <fgjv06@gmail.com>
2023-04-09 18:39:11 -0400

    JS - display loader until gif load

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let submitBtn = document.getElementById("submit-btn");
 
-let generateGif = () => {};
+let generateGif = () => {
+  // display loader until gif load
+  let loader = document.querySelector(".loader");
+  loader.style.display = "block";
+  document.querySelector(".wrapper").style.display = "none";
+};
 
 // Generate Gifs on screen load or when user clicks on submit
-submitBtn.addEventListener("click, generateGif");
+submitBtn.addEventListener("click", generateGif);
 window.addEventListener("load", generateGif);
