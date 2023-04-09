71b4a61128734f804e54a68b2951303bf66bf4d4
Fernando Jimenez <fgjv06@gmail.com>
2023-04-09 18:49:52 -0400

    JS - getting .wrapper element

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let generateGif = () => {
   let gifCount = 10;
   // API URL
   let finalURL = `https://api.giphy.com/v1/search?api_key=${apiKey}&q=${query}&limit=${gifCount}&offset=0&rating=g&lang=en`;
+  document.querySelector(".wrapper").innerHTML = "";
 };
 
 // Generate Gifs on screen load or when user clicks on submit
